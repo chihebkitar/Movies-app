bf59ecc6cfb8d575f19c41c8e0bb92b2f0685c74
chiheb <kitarchiheb11@gmail.com>
2024-06-24 10:46:21 +0200

    commit all files

---
+++ movies/src/main/java/com/app/movies/MovieController.java
@@ -0,0 +1,30 @@
+package com.app.movies;
+
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/api/v1/movies")
+public class MovieController {
+
+    @Autowired
+    private MovieService movieService;
+
+    @GetMapping
+    public ResponseEntity<List<Movie>> getAllMovies() {
+        return new ResponseEntity<>(movieService.allMovies(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{imdId}")
+    public ResponseEntity<Optional<Movie>> getMovieById(@PathVariable String imdId) {
+        return new ResponseEntity<>(movieService.SingleMovieById(imdId), HttpStatus.OK);
+    }
+}
